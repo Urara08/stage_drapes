bf3d192403240b0fd404bf076fc3d240beea8d78
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 20:21:57 +0900

    js_sample_2

--- js/script_effect.js
+++ js/script_effect.js
@@ -15,21 +15,17 @@ ScrollReveal().reveal('.js-sample',{
     origin: 'left', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
     distance: '50%',//移動距離（実数も可）
     mobile:false,//モバイルブラウザでの true（有効）/false（無効）
-    reset: true,//初期化するかどうか true/false
+    reset: falsee,//初期化するかどうか true/false
     rotate: { x: 0, y: 0, z: 100 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
     scale:'2'//開始時の大きさ（１＝100%）
 });
 });
 
-//［ScrollReveal］スクロールで出てくる
+//スクロールで幕があく
 $(function(){
-    ScrollReveal().reveal('.js-sample_2,.js-sample_3',{
-        duration: 1600, //完了までの時間
-        origin: 'bottom', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
-        distance: '100%',//移動距離（実数も可）
-        mobile:false,//モバイルブラウザでの true（有効）/false（無効）
-        reset:false,//初期化するかどうか true/false
-        rotate: { x: 100, y: 0, z: 0 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
-        scale:'2'//開始時の大きさ（１＝100%）
-    });
-    });
+    $(window).scroll(function() {
+        $(".js-sample_2").text($(this).scrollTop());
+        $("#curtain1_c2").animate({width:100, height:1000},1000).fadeOut(3000);
+        $("#curtain2_c2").animate({width:100, height:1000},1000).fadeOut(3000);
+
+    })})
