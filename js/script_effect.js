89229fd69865aa9cbcc7606e21f5be9cc5bb11ea
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 18:18:18 +0900

    ScrollReveal

--- js/script_effect.js
+++ js/script_effect.js
@@ -10,5 +10,12 @@ $(function() {
 
 //［ScrollReveal］スクロールで出てくる
 $(function(){
-ScrollReveal().reveal('.js-sample');
-            });
+ScrollReveal().reveal('.js-sample',{
+    duration: 1800, //完了までの時間
+    origin: 'left', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
+    distance: '50%',//移動距離（実数も可）
+    mobile:false,//モバイルブラウザでの true（有効）/false（無効）
+    reset: true,//初期化するかどうか true/false
+　　rotate: { x: 0, y: 0, z: 100 }//回転させながら表示 { x: 縦, y: 横, z: 水平 }
+});
+});
