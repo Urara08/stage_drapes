3a5e7a7a6321f3686f5a8929087a57fdfe5c1786
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 21:52:17 +0900

    調整

--- js/script_effect.js
+++ js/script_effect.js
@@ -11,13 +11,13 @@ $(function() {
 //［ScrollReveal］スクロールで出てくる
 $(function(){
 ScrollReveal().reveal('.js-sample',{
-    duration: 1800, //完了までの時間
-    origin: 'left', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
+    duration: 2000, //完了までの時間
+    origin: 'right', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
     distance: '50%',//移動距離（実数も可）
     mobile:false,//モバイルブラウザでの true（有効）/false（無効）
-    reset: falsee,//初期化するかどうか true/false
-    rotate: { x: 0, y: 0, z: 100 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
-    scale:'2'//開始時の大きさ（１＝100%）
+    reset: false,//初期化するかどうか true/false
+    rotate: { x: 0, y: 800, z: 0 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
+    scale:'5'//開始時の大きさ（１＝100%）
 });
 });
 
@@ -38,7 +38,7 @@ $(function(){
         distance: '100%',//移動距離（実数も可）
         mobile:false,//モバイルブラウザでの true（有効）/false（無効）
         reset: false,//初期化するかどうか true/false
-        rotate: { x: 0, y: 0, z: 800 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
-        scale:'0.4'//開始時の大きさ（１＝100%）
+        rotate: { x: 0, y:0, z: 800 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
+        scale:'0.2'//開始時の大きさ（１＝100%）
     });
     });
