bca5be74913d3c677c57aa938584bdd5a8f20d73
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 19:48:59 +0900

    js-sample_2準備

--- js/script_effect.js
+++ js/script_effect.js
@@ -20,3 +20,16 @@ ScrollReveal().reveal('.js-sample',{
     scale:'2'//開始時の大きさ（１＝100%）
 });
 });
+
+//［ScrollReveal］スクロールで出てくる
+$(function(){
+    ScrollReveal().reveal('.js-sample_2,.js-sample_3',{
+        duration: 1600, //完了までの時間
+        origin: 'bottom', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
+        distance: '100%',//移動距離（実数も可）
+        mobile:false,//モバイルブラウザでの true（有効）/false（無効）
+        reset:false,//初期化するかどうか true/false
+        rotate: { x: 100, y: 0, z: 0 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
+        scale:'2'//開始時の大きさ（１＝100%）
+    });
+    });
