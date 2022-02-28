e414bfab35dd8689efc1629cf917d32c92117782
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 17:41:14 +0900

    ［ScrollReveal］スクロールで出てくる

--- js/script_effect.js
+++ js/script_effect.js
@@ -2,6 +2,13 @@
 $(function() {
 
 	setTimeout(function open_curtain(){
+
     $("#curtain1").animate({width:30, height:1000},1000).fadeOut(3000);
     $("#curtain2").animate({width:30, height:1000},1000).fadeOut(3000);
+
 })})
+
+//［ScrollReveal］スクロールで出てくる
+$(function(){
+ScrollReveal().reveal('.js-sample');
+            });
