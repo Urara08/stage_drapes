9c6d6a9a96119fe66aed9e5dcafac8ad0ff1a37c
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-28 21:22:43 +0900

    スクロールで宝箱からでてくる

--- js/script_effect.js
+++ js/script_effect.js
@@ -29,3 +29,16 @@ $(function(){
         $("#curtain2_c2").animate({width:100, height:1000},1000).fadeOut(3000);
 
     })})
+
+//スクロールで宝箱から出てくる
+$(function(){
+    ScrollReveal().reveal('#js-sample_3',{
+        duration: 2500, //完了までの時間
+        origin: 'left', //distanceと一緒に使う。どの方向から来るか top/right/bottom/left
+        distance: '100%',//移動距離（実数も可）
+        mobile:false,//モバイルブラウザでの true（有効）/false（無効）
+        reset: false,//初期化するかどうか true/false
+        rotate: { x: 0, y: 0, z: 800 },//回転させながら表示する { x: 縦, y: 横, z: 水平 }
+        scale:'0.4'//開始時の大きさ（１＝100%）
+    });
+    });
